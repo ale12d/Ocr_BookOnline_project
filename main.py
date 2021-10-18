d04c5b9779256b9a841f1daa3effc91de6cacfd5
Alexy Dubois <alexy.dubois62@gmail.com>
2021-10-18 18:21:06 +0200

    Choix catégorie + ajout lien jpg et catégories au fichier csv

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import csv
 
 i = 0
+n = 0
+
 données = []
 en_tete = ("product_page_url", "universal_ product_code (upc)", "title", "price_including_tax", "price_excluding_tax",
            "number_available", "product_description", "category", "review_rating", "image_url")
@@ -32,40 +34,43 @@ with open('Données.csv', 'w') as fichier_csv:
 
     del listCat[0]
     del listURLcat[0]
-
-    for c in range(len(listURLcat)):
-
-        pagecat = requests.get(listURLcat[i])
-        soupcat = BeautifulSoup(pagecat.content, 'html.parser')
-        i = i + 1
-        listCatalogue = soupcat.find_all('div', attrs={'class': "image_container"})
-
-        for b in listCatalogue:
+    print("Choisir une catégorie")
+    print(listCat)
+    CategoryChoise = input()
 
 
-            # Code html du livre
-            urlBook = "http://books.toscrape.com/catalogue/" + b.find('a')["href"][9:]
+    for n in range(len(listCat)):
+        if CategoryChoise == listCat[n]:
+            pagecat = requests.get(listURLcat[n])
+            soupcat = BeautifulSoup(pagecat.content, 'html.parser')
+            listCatalogue = soupcat.find_all('div', attrs={'class': "image_container"})
 
+            for b in listCatalogue:
+                # Code html du livre
+                urlBook = "http://books.toscrape.com/catalogue/" + b.find('a')["href"][9:]
 
-            print(urlBook)
+                pagebook = requests.get(urlBook)
+                soupbook = BeautifulSoup(pagebook.content, 'html.parser')
 
-            pagebook = requests.get(urlBook)
-            soupbook = BeautifulSoup(pagebook.content, 'html.parser')
+                # Extraction code html de la barre d'infos
+                info = soupbook.find('table', attrs={'class': 'table table-striped'})
 
-            # Extraction code html de la barre d'infos
-            info = soupbook.find('table', attrs={'class': 'table table-striped'})
+                # Extraction des infos
+                title = soupbook.find('li', attrs={'class': "active"})
+                upc = info.find(text="UPC").findNext('td').contents[0]
+                priceInc = info.find(text="Price (incl. tax)").findNext('td').contents[0]
+                priceExc = info.find(text="Price (incl. tax)").findNext('td').contents[0]
+                nbAAv = info.find(text="Availability").findNext('td').contents[0]
+                descPro = soupbook.find(text="Product Description").findNext('p').contents[0]
+                img = soupbook.find('div', attrs={'class': "item active"}).find('img')["src"]
+                rating = soupbook.find('p', attrs={'class': "instock availability"}).find_next_sibling('p')
 
-           # Extraction des infos
-            title = soupbook.find('li', attrs={'class': "active"})
-            upc = info.find(text="UPC").findNext('td').contents[0]
-            priceInc = info.find(text="Price (incl. tax)").findNext('td').contents[0]
-            priceExc = info.find(text="Price (incl. tax)").findNext('td').contents[0]
-            nbAAv = info.find(text="Availability").findNext('td').contents[0]
-            #descPro = soupbook.find(text="Product Description").findNext('p').contents[0]
-            category = 0
+                print(rating)
+                print(title.get_text())
 
-            # print(title.get_text())
+                # Ecriture des infos extraite dans le fichier csv
+                données = [urlBook, upc, title.get_text(), priceExc, priceInc, nbAAv, descPro, CategoryChoise, rating, img]
+                writer.writerows([données])
+        elif CategoryChoise in listCat:
+            n + 1
 
-            # Ecriture des infos extraite dans le fichier csv
-            données = [urlBook, upc, title.get_text(), priceExc, priceInc, nbAAv, ]
-            writer.writerows([données])
