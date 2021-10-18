895cc1362e2f2c4052ed713b373209184500a896
Alexy Dubois <alexy.dubois62@gmail.com>
2021-10-18 16:30:06 +0200

    listage des livres de toutes les catégories

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
+i = 0
 données = []
 en_tete = ("product_page_url", "universal_ product_code (upc)", "title", "price_including_tax", "price_excluding_tax",
            "number_available", "product_description", "category", "review_rating", "image_url")
@@ -11,42 +12,60 @@ url = "http://books.toscrape.com/"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
+listCat = []
+listURLcat = []
+
 # Création du fichier csv + affichage de l'en-tête
 with open('Données.csv', 'w') as fichier_csv:
     writer = csv.writer(fichier_csv, delimiter=',')
     writer.writerow(en_tete)
 
-    # Code html des catégories catalogue des livres
+    # Code html des catégories et catalogues des livres
     listCatalogue = soup.find_all('div', attrs={'class': "image_container"})
-    listCategory = soup.find_all('ul', attrs={'class': "nav nav-list"})
+    listCategory = soup.find_all('div', attrs={'class': "side_categories"})
 
     for a in listCategory:
         for li in a.findAll('li'):
             urlCategory = "http://books.toscrape.com/" + li.find('a')["href"]
-            print(urlCategory)
-            print(li.get_text())
+            listCat.append(li.get_text().replace("\n", "").strip())
+            listURLcat.append(urlCategory)
+
+    del listCat[0]
+    del listURLcat[0]
+
+    for c in range(len(listURLcat)):
+
+        pagecat = requests.get(listURLcat[i])
+        soupcat = BeautifulSoup(pagecat.content, 'html.parser')
+        i = i + 1
+        listCatalogue = soupcat.find_all('div', attrs={'class': "image_container"})
+
+        for b in listCatalogue:
 
 
-    for b in listCatalogue:
-        # Code html du livre
-        urlBook = "http://books.toscrape.com/" + b.find('a')["href"]
-        pagebook = requests.get(urlBook)
-        soupbook = BeautifulSoup(pagebook.content, 'html.parser')
+            # Code html du livre
+            urlBook = "http://books.toscrape.com/catalogue/" + b.find('a')["href"][9:]
 
-        # Extraction code html de la barre d'infos
-        info = soupbook.find('table', attrs={'class': 'table table-striped'})
 
-        # Extraction des infos
-        title = soupbook.find('li', attrs={'class': "active"})
-        upc = info.find(text="UPC").findNext('td').contents[0]
-        priceInc = info.find(text="Price (incl. tax)").findNext('td').contents[0]
-        priceExc = info.find(text="Price (incl. tax)").findNext('td').contents[0]
-        nbAAv = info.find(text="Availability").findNext('td').contents[0]
-        descPro = soupbook.find(text="Product Description").findNext('p').contents[0]
-        category = 0
+            print(urlBook)
 
-        # print(title.get_text())
+            pagebook = requests.get(urlBook)
+            soupbook = BeautifulSoup(pagebook.content, 'html.parser')
 
-        # Ecriture des infos extraite dans le fichier csv
-        données = [urlBook, upc, title.get_text(), priceExc, priceInc, nbAAv, descPro, ]
-        writer.writerows([données])
+            # Extraction code html de la barre d'infos
+            info = soupbook.find('table', attrs={'class': 'table table-striped'})
+
+           # Extraction des infos
+            title = soupbook.find('li', attrs={'class': "active"})
+            upc = info.find(text="UPC").findNext('td').contents[0]
+            priceInc = info.find(text="Price (incl. tax)").findNext('td').contents[0]
+            priceExc = info.find(text="Price (incl. tax)").findNext('td').contents[0]
+            nbAAv = info.find(text="Availability").findNext('td').contents[0]
+            #descPro = soupbook.find(text="Product Description").findNext('p').contents[0]
+            category = 0
+
+            # print(title.get_text())
+
+            # Ecriture des infos extraite dans le fichier csv
+            données = [urlBook, upc, title.get_text(), priceExc, priceInc, nbAAv, ]
+            writer.writerows([données])
