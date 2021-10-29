09dbcb0e379030d829f5c0170c069e5e647079d3
Alexy Dubois <alexy.dubois62@gmail.com>
2021-10-29 10:24:05 +0200

    Ajout fonctions + commentaires

--- fonc.py
+++ fonc.py
@@ -1,27 +1,62 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 def get_cat_and_url(list_ctgrs):
+
+    """Returns all categories their url
+
+        Parameters:
+        list_ctgrs (list_categories): sidebar html code
+
+        Returns:
+        list (list_cat): list of categories
+        listURL (listURL_cat): categories url list
+
+
+       """
+
     list = []
     listURL = []
+
     for a in list_ctgrs:
         for li in a.findAll('li'):
             url_category = "http://books.toscrape.com/" + li.find('a')["href"]
             list.append(li.get_text().replace("\n", "").strip())
             listURL.append(url_category)
+
     del list[0]
     del listURL[0]
+
     return list, listURL
 
+
 def get_product_info(i):
+
+    """Returns all information about the book
+
+        Parameters:
+        i (b): number of iterations to extract information from books
+
+        Returns:
+        url_book
+        title
+        upc
+        price_inc
+        price_exc
+        nb_avail
+        img
+        rating
+        pro_desc
+
+       """
+
     url_book = "http://books.toscrape.com/catalogue/" + i.find('a')["href"][9:]
     pagebook = requests.get(url_book)
     soupbook = BeautifulSoup(pagebook.content, 'html.parser')
 
-    # Extraction code html de la barre d'infos
     info = soupbook.find('table', attrs={'class': 'table table-striped'})
 
-    # Extraction des infos
     title = soupbook.find('li', attrs={'class': "active"})
     upc = info.find(text="UPC").findNext('td').contents[0]
     price_inc = info.find(text="Price (incl. tax)").findNext('td').contents[0]
@@ -35,9 +70,28 @@ def get_product_info(i):
     else:
         pro_desc = "no description"
 
-    return url_book, title, upc, price_inc, price_exc, nb_avail, img , rating, pro_desc
+    return url_book, title, upc, price_inc, price_exc, nb_avail, img, rating, pro_desc
+
 
 def get_additional_page(soupcat_fo, listURL_cat_fo, n_fo, additional_page_fo, retain_fo, add_write_fo, pagecat_sup):
+    """Returns the number of additional pages and its url
+
+        Parameters:
+        soupcat_fo (soupcat): code html of the category page
+        listURL_cat_fo (listURL_cat): categories url list
+        n_fo (n): number of iterations to extract all books on a page
+        additional_page_fo (additional_page): number of pages >= page2
+        retain_fo (retain): retained additional page
+        add_write_fo (add_write): write mode of csv file
+        pagecat_sup (pagecat): page of the category page
+
+        Returns:
+        retain_fo (retain): retained additional page
+        add_write_fo (add_write): write mode of csv file
+        pagecat_sup (pagecat): page of the category page
+        additional_page_fo (additional_page): number of pages >= page2
+
+       """
     pager = soupcat_fo.find('li', attrs={'class': "next"})
 
     if pager:
@@ -51,3 +105,20 @@ def get_additional_page(soupcat_fo, listURL_cat_fo, n_fo, additional_page_fo, re
         retain_fo = 0
 
     return retain_fo, add_write_fo, pagecat_sup, additional_page_fo
+
+def get_pic(title_fo, img_fo):
+    """download jpg picture of books
+
+        Parameters:
+        title_fo (title): title of the book
+        img_fo (img): images url
+
+
+        Returns:
+
+
+       """
+
+    img_data = requests.get("http://books.toscrape.com/" + img_fo[6:]).content
+    with open("Image/" + title_fo.get_text().replace('/', '-').replace(':', '-').replace('\"', ' ').replace('*',' ').replace('?', ' ') + '.jpg', 'wb') as handler:
+        handler.write(img_data)
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import requests
-from bs4 import BeautifulSoup
 from fonc import *
 import csv
 import os
 
+n = -1
 retain = 0
 additional_page = 0
 progress = 0
@@ -13,53 +12,54 @@ pagecat = 0
 header = ("product_page_url", "universal_ product_code (upc)", "title", "price_including_tax", "price_excluding_tax",
           "number_available", "product_description", "category", "review_rating", "image_url")
 
-# Code html du site
+# html home Books to Scrape
 url = "http://books.toscrape.com/"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-# Création du dossier Livres
+# creation of the books and images folder
 if not os.path.exists('Books'):
     os.mkdir('Books', 0o777)
 
 if not os.path.exists('Image'):
     os.mkdir('Image', 0o777)
 
-# Code html des catégories et catalogues des livres
+# html code for categories
 list_categories = soup.find_all('div', attrs={'class': "side_categories"})
 
-# Ajout des catégories et de ses URL dans une liste
-
+# Returns all categories their url
 list_cat, listURL_cat = get_cat_and_url(list_categories)
 
-for n in range(30 + len(list_cat)):
+while n < additional_page + len(list_cat) -1:
+    n = n+1
 
-    # Création du fichier csv + affichage de l'en-tête
 
     if retain == 0:
-        pagecat = requests.get(listURL_cat[n - additional_page])
+        pagecat = requests.get(listURL_cat[n - additional_page ])
         add_write = 'w'
     soupcat = BeautifulSoup(pagecat.content, 'html.parser')
     list_catalog = soupcat.find_all('div', attrs={'class': "image_container"})
 
-    with open("Books/" + list_cat[n - additional_page] + ".csv", add_write, encoding="utf-8", newline="") as fichier_csv:
+    # Creation of csv files + header writing
+    with open("Books/" + list_cat[n - additional_page] + ".csv", add_write, encoding="utf-8", newline="") as fichier_csv :
         writer = csv.writer(fichier_csv, delimiter=',',)
         if retain == 0:
             writer.writerow(header)
 
         for b in list_catalog:
 
+            # Returns all information about the book
             url_book, title, upc, price_inc, price_exc, nb_avail, img , rating, pro_desc = get_product_info(b)
-            # print(title.get_text())
 
-            img_data = requests.get(url + img[6:]).content
-            with open("Image/" + title.get_text().replace('/', '-').replace(':', '-').replace('\"', ' ').replace('*', ' ').replace('?', ' ') + '.jpg', 'wb') as handler:
-                handler.write(img_data)
+            # print(title.get_text()) --> print titles
+
+            # download jpg picture of books
+            get_pic(title, img)
 
             progress = progress + 1
             print(str(progress / 10) + '%')
 
-            # Ecriture des infos extraite dans le fichier csv
+            # Writing of the information extracted in the csv file
             data = [url_book, upc, title.get_text(), price_exc, price_inc, nb_avail, pro_desc, list_cat[n - additional_page], rating, img]
             writer.writerows([data])
 
